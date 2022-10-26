03942965b059c23a41d9e95a226bf4efed430ef6
Zellenon <Zellenon@gmail.com>
2022-10-25 23:52:13 -0500

    convolution basics

---
+++ convolution/Assets/Convolution.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Complex = System.Numerics.Complex;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+//using MathNet.Numerics.IntegralTransforms;
+
+public class Convolution : MonoBehaviour
+{
+
+
+    [SerializeField] private Material defaultLineMaterial;
+    [SerializeField] private TextMeshPro OriginLabel;
+    [SerializeField] private PlotObj topPlot, topRightPlot, bottomPlot;
+    // [SerializeField] private EquationText eqnText;
+    // [SerializeField] private Slider _magSlider, _freqSlider, _phaseSlider;
+
+
+    // xtlp - x-top-left-plot; ect for x,y,width,height
+    // top-right-plot; ect
+    // bottom-plot
+    private float xtlp, ytlp, wtlp, htlp,
+    xtrp, ytrp, wtrp, htrp,
+    xbp, ybp, wbp, hbp;
+
+    public float _funct1mag, _funct1freq,
+    _funct2mag, _funct2freq;
+
+
+
+    private int Nfft, Nfreq;
+    private float _Fs, _tmax;
+    public float _mag, _freq, _phase;
+
+
+    private Vector2[] _func1pts, _funct2pts, _resultpts;
+
+    //private LineRenderer _func1, _funt2, _result;
+    
+    
+
+    void Awake(){
+        
+        float ymax = Camera.main.orthographicSize;
+        float xmax = ymax*Camera.main.aspect;
+        
+        ymax = ymax - 0.5f;
+        xmax = xmax - 0.5f;
+
+
+        Debug.LogFormat("xmax: {0}, ymax: {1}", xmax, ymax);
+
+        // we will ue the top left quadrant of the screen
+        float xymargin = 0.5f;
+
+
+         xtlp = 0; 
+         ytlp = (1f / 3f) * ymax - 0.3f;
+        // w2 = xmax - xymargin; h2 = ymax * 2f / 3f - xymargin;
+
+         xtrp = 0; 
+         ytrp = 2f / 3f * ymax - 0.3f;
+        // w1 = xmax - xymargin; h1 = ymax / 3f - xymargin;
+
+        Nfft = 1024;
+        _tmax = 5f;
+        _Fs = Nfft/(2 * _tmax); // sampling rate
+        Nfreq = 50;    // how many harmonics to display
+
+        _mag = 1.5f; _freq = 1.1f; _phase = 0;
+
+        
+        
+        topPlot.CreateGrid(-xmax/2, ymax/2, (xmax/2)-0.2f, ymax/2, Color.grey, defaultLineMaterial);
+
+        topRightPlot.CreateGrid(xmax/2, ymax/2, (xmax/2) - 0.2f, ymax/2, Color.grey, defaultLineMaterial);
+        //OriginLabel.gameObject.SetActive(false);
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+        
+        //topPlot.Update();
+        
+    }
+
+    
+}
---
+++ convolution/Assets/PlotObj.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlotObj : MonoBehaviour
+{
+    public float _x0, _y0, _width, _height;   // center and size of the plot region
+    
+    private float _xscale, _yscale;
+    
+    // Defined as a list so we can create line shapes with different points
+    private List<Vector3> points = new List<Vector3>();
+
+
+    [SerializeField] LineRenderer lr;
+    [SerializeField] private Color _axisColor;
+    [SerializeField] Material defaultLineMat;
+    
+
+    void Awake(){
+        lr = GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+        lr.widthMultiplier = 0.2f;
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+         
+    }
+
+
+    public void CreateGrid(float x0, float y0, float width, float height, Color axisColor, Material lineMaterial){
+        
+        points.Add(new Vector3(((x0-width)),(y0),0.0f));
+        points.Add(new Vector3((x0),(y0),0.0f));
+        points.Add(new Vector3((x0),(y0+(height)),0.0f));
+        points.Add(new Vector3((x0),(y0),0.0f));
+        points.Add(new Vector3(((x0+width)),(y0),0.0f));
+
+        // Tell it to make it default line material
+        lr.material = lineMaterial;
+        
+        
+
+        // For whatever reason, Unity requires user to define a gradient instead of just telling it a solid color >:( .... so we create a gradient from one color to another
+        //      color (otherwise known as a solid color)
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(axisColor, 0.0f), new GradientColorKey(axisColor, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        this.lr.colorGradient = gradient;
+        
+
+        lr.positionCount = points.Count;
+
+    }
+
+    //not used yet
+    public void CreateDottedLine(float x0, float y0, float x1, float y1, Color axisColor, Material lineMaterial){
+        
+
+        // points.Add(new Vector3(((x0-width)),(y0),0.0f));
+        // points.Add(new Vector3((x0),(y0),0.0f));
+        // lr.positionCount = points.Count;
+        // lr.material = new Material(Shader.Find("Sprites/Default"));
+        //this.Update();
+
+    }
+
+
+
+    // Update is called once per frame
+    public void Update()
+    {    
+        // loop over the points to make a grid of points to connect the line to
+        for (int i = 0; i < points.Count; i++){
+            lr.SetPosition(i, points[i]);
+        }
+
+    }
+
+}
