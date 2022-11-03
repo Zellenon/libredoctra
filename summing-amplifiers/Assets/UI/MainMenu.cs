ec7ead4608b75c3039e126de7c9af0f9f28a3677
Zellenon <Zellenon@gmail.com>
2022-11-03 02:19:52 -0500

    revamped main menu

---
+++ summing-amplifiers/Assets/UI/MainMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private UIDocument _doc;
+    private Button _mixerButton;
+    private Button _gameButton;
+
+    private void Awake()
+    {
+        _doc = GetComponent<UIDocument>();
+        _mixerButton = _doc.rootVisualElement.Q<Button>("Game1");
+        _gameButton = _doc.rootVisualElement.Q<Button>("Game2");
+
+        _mixerButton.clicked += SinewaveFFT;
+        _gameButton.clicked += PlayGame;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("PlayGame");
+    }
+
+
+    public void SinewaveFFT()
+    {
+        SceneManager.LoadScene("SinewaveFFT");
+    }
+}
