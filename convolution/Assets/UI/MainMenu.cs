778cbad623f6792468c40f1d9b1b0ea7b11a7e2b
Boofsnorf <boofsnorf@pop-os.localdomain>
2022-11-03 14:01:17 -0500

    Fixed UI forwarding

--- convolution/Assets/UI/MainMenu.cs
+++ convolution/Assets/UI/MainMenu.cs
@@ -39,7 +39,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("ConvolutionTest");
     }
 
 
