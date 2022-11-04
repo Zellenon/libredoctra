a97baf4afaa707ad4e623b8a5bee0b3a73dc9e72
Zellenon <Zellenon@gmail.com>
2022-11-03 20:48:34 -0500

    init commit

---
+++ convolution/Assets/Scripts/AbstractWave.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+using TMPro;
+
+public class wave : MonoBehaviour
+{
+
+}
