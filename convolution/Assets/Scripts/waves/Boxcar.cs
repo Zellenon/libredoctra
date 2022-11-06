4b140ab11792460d7a1a2b213ab20988eb20813d
Boofsnorf <boofsnorf@pop-os.localdomain>
2022-11-05 22:18:02 -0500

    dumb boxcar fixes

--- convolution/Assets/Scripts/waves/Boxcar.cs
+++ convolution/Assets/Scripts/waves/Boxcar.cs
@@ -4,7 +4,7 @@ using System;
 
 public class Boxcar : AbstractWave {
 
-    public override float get(int i) {
+    public override float get(int x) {
         return (x >= 0 && x <= 1) ? 1.0f : 0.0f;
     }
 }
