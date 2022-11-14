9d03e1227a746645db3938cb8fe396d2592fdb4f
Boofsnorf <boofsnorf@pop-os.localdomain>
2022-11-13 20:44:11 -0600

    fixed redundant imports

--- convolution/Assets/Scripts/waves/Sawtooth.cs
+++ convolution/Assets/Scripts/waves/Sawtooth.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using System;
 
 public class Sawtooth : AbstractWave {
 
--- convolution/Assets/Scripts/waves/Triangle.cs
+++ convolution/Assets/Scripts/waves/Triangle.cs
@@ -1,10 +1,9 @@
 using System;
 using UnityEngine;
-using System;
 
 public class Triangle : AbstractWave {
 
     public override float get(float i) {
-        return 2 * (_freq * i - Mathf.Floor(_freq * i + 1.0/2.0));
+        return 2.0f * (_freq * i - Mathf.Floor(_freq * i + 1.0f/2.0f));
     }
 }
