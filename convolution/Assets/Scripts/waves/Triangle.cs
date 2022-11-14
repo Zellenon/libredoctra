a40a14414465beaca2e3a2963295a01108cd8f6e
Boofsnorf <boofsnorf@pop-os.localdomain>
2022-11-13 21:15:43 -0600

    added abs

--- convolution/Assets/Scripts/waves/Triangle.cs
+++ convolution/Assets/Scripts/waves/Triangle.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 public class Triangle : AbstractWave {
 
     public override float get(float i) {
-        return 2.0f * (_freq * i - Mathf.Floor(_freq * i + 1.0f/2.0f));
+        return 2.0f * Mathf.Abs(_freq * i - Mathf.Floor(_freq * i + 1.0f/2.0f));
     }
 }
