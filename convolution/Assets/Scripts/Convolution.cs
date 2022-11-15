3b98e9fc3aa05c112793179edb1e829217c8f58e
Boofsnorf <boofsnorf@pop-os.localdomain>
2022-11-14 22:43:56 -0600

    floats everywhere

--- convolution/Assets/Scripts/Convolution.cs
+++ convolution/Assets/Scripts/Convolution.cs
@@ -229,7 +229,7 @@ public class Convolution : MonoBehaviour
     private void ConvolveCallback(ChangeEvent<float> evt) {
         print(evt.newValue);
         int offset = (int) evt.newValue;
-        lineContainer4.transform.position = new Vector3((offset/48),(0),0f); // why 48?
+        lineContainer4.transform.position = new Vector3((offset/48.0f),(0.0f),0f); // why 48?
         float multiplier = MAX_X / STEPCOUNT;
         float convsum = 0.0f;
         for (int i = 0; i <= offset; i++) {
