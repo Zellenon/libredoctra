9971ddf4ae3a668b14061798905a1c21e15e3292
RupB <rupanti-bose@uiowa.edu>
2022-11-15 01:33:14 -0600

    Sliders added

--- summing-amplifiers/Assets/Scripts/Match_IF.cs
+++ summing-amplifiers/Assets/Scripts/Match_IF.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Match_IF : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     private Slider scaleSlider;
+    public float scaleMinValue;
+    public float scaleMaxValue;
+    
+    private Slider scaleSliderX;
+    public float scaleXMinValue;
+    public float scaleXMaxValue;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        scaleSlider = GameObject.Find("MagnitudeSlider").GetComponent<Slider>();
+        scaleSlider.minValue = scaleMinValue;
+        scaleSlider.maxValue = scaleMaxValue;
+        scaleSlider.onValueChanged.AddListener(ScaleSliderUpdate);
+
+        scaleSliderX = GameObject.Find("FreqSlider").GetComponent<Slider>();
+        scaleSliderX.minValue = scaleXMinValue;
+        scaleSliderX.maxValue = scaleXMaxValue;
+        scaleSliderX.onValueChanged.AddListener(ShiftSliderUpdate);
     }
+    
+    void ScaleSliderUpdate(float value){transform.localScale = new Vector2(0.304793f,value);}
+    void ShiftSliderUpdate(float value) {transform.localPosition = new Vector2(value,-1.91f);}
+   
+
+    
 }
