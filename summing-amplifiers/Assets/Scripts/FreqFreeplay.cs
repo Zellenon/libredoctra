bd53e5e0302857c675c752f8711af3fc5d26051c
RupB <rupanti-bose@uiowa.edu>
2022-12-05 15:23:05 -0600

    Document page edited

--- summing-amplifiers/Assets/Scripts/FreqFreeplay.cs
+++ summing-amplifiers/Assets/Scripts/FreqFreeplay.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class FreqFreeplay : MonoBehaviour
 {
-        public float freqSub=1;
+        
         public float freqAdd=1;
+        public float ampMul=1;
         private GameObject wave;
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,10 @@ public class FreqFreeplay : MonoBehaviour
     void Update()
     {
         freqAdd=wave.GetComponent<MultiplyWaves>().freqAdd;
-        //freqSub=wave.GetComponent<MultiplyWaves>().freqSub;
+        ampMul=wave.GetComponent<MultiplyWaves>().ampMul;
+        
         transform.position = new Vector2(freqAdd+3.75f, -2.35f);
-        //void ShiftSliderUpdate(float value) {transform.localPosition = new Vector2(value,-3.49f);}
+        //transform.localScale=new Vector2(0.065625f,ampMul);
         
     }
 }
